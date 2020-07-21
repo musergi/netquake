df56a1d24109491545abcd9fa7822170e4a1c0a5
musergi <sergi.mus@gmail.com>
2020-07-21 12:19:58 +0200

    Implemented trace filtering

---
+++ netquake/commands/get_eventfull_traces.py
@@ -0,0 +1,28 @@
+import os
+import tqdm
+import pandas as pd
+from obspy.core import stream
+
+def contains_event(trace, df):
+    station_code = trace.stats.station
+    channel_code = trace.stats.channel[1:]
+    start = trace.stats.starttime
+    end = trace.stats.endtime
+    possible_picks = df[
+        (df['station_code'] == station_code) &
+        (df['channel_code'] == channel_code) &
+        (df['time'] > start) &
+        (df['time'] < end)]
+    return len(possible_picks) > 0
+
+def run(input_paths, output_paths):
+    df = pd.read_csv(input_paths[0])
+    signals_folder = input_paths[1]
+    dump_folder = output_paths[0]
+    
+    index = 0
+    for filename in tqdm.tqdm(os.listdir(signals_folder)):
+        file_stream = stream.read(os.path.join(signals_folder, filename))
+        for trace in file_stream:
+            if contains_event(trace, df):
+                trace.write(os.path.join(dump_folder, f'{index}.mseed'), format='MSEED')
