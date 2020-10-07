cd53c11f3f61bb9b3bc77238472f2a273e131579
musergi <sergi.mus@gmail.com>
2020-10-07 19:59:10 +0200

    Added filtering to results

--- netquake/commands/apply_network.py
+++ netquake/commands/apply_network.py
@@ -4,6 +4,14 @@ import matplotlib.pyplot as plt
 from tensorflow.keras.models import load_model
 
 
+def smoothed(array, window_size):
+    result_length = len(array) - window_size + 1
+    result = np.zeros(result_length)
+    for i in range(result_length):
+        result[i] = np.mean(array[i:i + window_size])
+    return result
+
+
 def rolling_window(a, window, step_size):
     shape = a.shape[:-1] + (a.shape[-1] - window + 1 - step_size + 1, window)
     strides = a.strides + (a.strides[-1] * step_size,)
@@ -24,8 +32,12 @@ def run(network_path, trace_path):
     print('Result shape', result.shape)
     result = np.reshape(result, result.shape[:-1])
 
-    plt.subplot(2, 1, 1)
+    plt.subplot(4, 1, 1)
     plt.plot(trace.data)
-    plt.subplot(2, 1, 2)
+    plt.subplot(4, 1, 2)
     plt.plot(result)
+    plt.subplot(4, 1, 3)
+    plt.plot(smoothed(result, 16))
+    plt.subplot(4, 1, 4)
+    plt.plot(smoothed(result, 64))
     plt.show()
